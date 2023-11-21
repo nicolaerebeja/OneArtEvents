b9b58c42eb692d644c21a69667de31bab1c4d475
nicolaerebeja <rebejanicolae@gmail.com>
2023-11-21 16:08:42 +0100

    events

--- website/controllers/admin/eventsController.py
+++ website/controllers/admin/eventsController.py
@@ -85,7 +85,9 @@ def getEvents():
             'date': str(event.date),
             'time': str(event.time),
             'event_type': event.event_type,
-            'location': event.location_id,
+            'location': event.location.name,
+            'location_street': event.location.street,
+            'location_details': event.location.details,
             'photo_video': event.photo_video_id,
             'music': event.music_id,
             'moderators': event.moderators_id,
@@ -99,3 +101,124 @@ def getEvents():
 
     # Returneaza evenimentele sub forma de JSON fara obiect exterior
     return jsonify(events_json)
+
+
+@login_required
+def getLocation():
+    term = request.args.get('term', '').lower()
+
+    locations = Location.query.filter(
+        (Location.name.ilike(f'%{term}%')) | (Location.street.ilike(f'%{term}%'))
+    ).all()
+
+    locations_json = [
+        {
+            'id': location.id,
+            'name': location.name,
+            'street': location.street,
+            'details': location.details,
+        }
+        for location in locations
+    ]
+
+    return jsonify(locations_json)
+
+
+@login_required
+def getDancers():
+    dancers = User.query.all()
+
+    dancers_json = [
+        {
+            'id': dancer.id,
+            'name': dancer.first_name,
+        }
+        for dancer in dancers
+    ]
+
+    return jsonify(dancers_json)
+
+
+@login_required
+def modificaDetaliiEveniment():
+    if request.method == 'POST':
+        try:
+            id = request.form.get('id')
+
+            date = request.form.get('date')
+            date_obj = datetime.strptime(date, '%Y-%m-%d').date()
+
+            time = request.form.get('time')
+            time_str = unquote(time)
+            if time_str:
+                time_obj = datetime.strptime(time_str, '%H:%M:%S').time()
+            else:
+                time_obj = None
+
+            tip_eveniment = request.form.get('tip_eveniment')
+            tip_eveniment_decoded = unquote(tip_eveniment)
+
+            miri = request.form.get('miri')
+            miri_decoded = unquote(miri)
+
+            contacte_miri = request.form.get('contacte_miri')
+            contacte_miri_decoded = unquote(contacte_miri)
+
+            detalii_nunta = request.form.get('detalii_nunta')
+            detalii_nunta_decoded = unquote(detalii_nunta)
+
+            dancers = request.form.get('dancers')
+            dancers_decoded = unquote(dancers)
+
+            location = request.form.get('location')
+            street = request.form.get('street')
+            location_details = request.form.get('location_details')
+            location_db = Location.query.filter_by(name=location, street=street, details=location_details).first()
+            if location_db:
+                location_id = location_db
+            else:
+                new_location = Location(
+                    name=location,
+                    street=street,
+                    details=location_details,
+                )
+                db.session.add(new_location)
+                db.session.commit()
+                location_id = new_location
+
+            event = Event.query.get(id)
+            if event:
+                if event.date != date_obj:
+                    event.date = date_obj
+                if event.time != time_obj:
+                    event.time = time_obj
+                if event.event_type != tip_eveniment_decoded:
+                    event.event_type = tip_eveniment_decoded
+                if event.mires != miri_decoded:
+                    event.mires = miri_decoded
+                if event.contacts != contacte_miri_decoded:
+                    event.contacts = contacte_miri_decoded
+                if event.details != detalii_nunta_decoded:
+                    event.details = detalii_nunta_decoded
+                if event.dancers != dancers_decoded:
+                    event.dancers = dancers_decoded
+                if event.location != location_id:
+                    event.location = location_id
+
+                db.session.commit()
+
+                return jsonify({'success': True, 'message': 'Eveniment modificat cu succes.'})
+
+            else:
+                return jsonify({'success': False, 'message': 'Evenimentul nu a fost găsit.'})
+
+        except Exception as e:
+            db.session.rollback()
+            return jsonify({'success': False, 'message': 'Eroare la modificarea evenimentului: {}'.format(str(e))})
+
+
+    else:
+        event_id = request.args.get('id')
+        event = Event.query.get(event_id)
+
+        return render_template("admin/modifica-eveniment.html", event=event)
--- website/views.py
+++ website/views.py
@@ -3,7 +3,8 @@ from flask import Blueprint
 from .controllers.admin.AdminHomeController import adminHome
 from .controllers.admin.UserController import userIndex
 
-from .controllers.admin.eventsController import eventsIndex, adaugaEveniment, getEvents
+from .controllers.admin.eventsController import eventsIndex, adaugaEveniment, getEvents, getLocation, getDancers, \
+    modificaDetaliiEveniment
 
 from .controllers.HomeController import home
 
@@ -14,12 +15,9 @@ views.route('/users', methods=['GET', 'POST'])(userIndex)
 
 views.route('/events', methods=['GET'])(eventsIndex)
 
-views.route('/adaugaEveniment', methods=[ 'POST'])(adaugaEveniment)
-views.route('/getEvents', methods=[ 'GET'])(getEvents)
+views.route('/adaugaEveniment', methods=['POST'])(adaugaEveniment)
+views.route('/modificaDetaliiEveniment', methods=['GET', 'POST'])(modificaDetaliiEveniment)
 
-
-
-# views.route('/', methods=['GET', 'POST'])(home)
-
-
-
+views.route('/getEvents', methods=['GET'])(getEvents)
+views.route('/getLocation', methods=['GET'])(getLocation)
+views.route('/getDancers', methods=['GET'])(getDancers)
